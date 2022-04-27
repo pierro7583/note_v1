33cdbd64cd3394027d7f35635489aa79b3d06cc7
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-27 19:06:56 +0200

    clean file

--- note_v1/src/App.js
+++ note_v1/src/App.js
@@ -1,21 +1,31 @@
-import React from "react";
-import './App.css';
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Note from "./components/Note.jsx";
-import notes from "./components/notes.js";
-import CreateArea from "./components/CreateArea.jsx";
-
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <CreateArea />
       <Note key={1} title="Note title" content="Note content" />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+// Note instruction step by step
+
+//1. Implémentez la fonctionnalité d'ajout de note.
+//- Crée une constante qui garde une trace du titre et du contenu.
+//- Renvoyez la nouvelle note à l'application.
+//- Ajouter une nouvelle note à un tableau.
+//- Prendre un tableau et restituer des composants Note séparés pour chaque élément.
+
+//2. Implémentez la fonctionnalité de suppression de note.
+//- Rappel du composant Note pour déclencher une fonction de suppression.
+//- Utilisez la fonction de filtre pour filtrer l'élément qui doit être supprimé.
+//- Transmettez un identifiant au composant Note, retransmettez-le à l'application lors de la suppression.
