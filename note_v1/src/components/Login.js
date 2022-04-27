4262a7d75f0ead875433b17447da0e52b4151771
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-27 13:58:56 +0200

    Update
    Add CreateArea + style

---
+++ note_v1/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login(){
+    return (
+    
+        <div className="container">
+        <h1>Hello</h1>
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+
+    );
+}
+
+export default Login;
