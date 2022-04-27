7d5b9f0f1d17c58fffdccfc8badb4beb5a118a2e
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-27 12:55:20 +0200

    Update App.js
    Arrow function

--- note_v1/src/App.js
+++ note_v1/src/App.js
@@ -5,16 +5,20 @@ import Footer from "./components/Footer.jsx";
 import Note from "./components/Note.jsx";
 import notes from "./components/notes.js";
 
-function createNotes(noteItem){
-  return <Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>;
-}
+
 
 function App() {
   return (
     <div>
       <Header/>
      {/* props notes array */}
-     {notes.map(createNotes)}
+     {notes.map(noteItem =>(
+     <Note 
+  key={noteItem.key} 
+  title={noteItem.title} 
+  content={noteItem.content}
+  />
+))}
       <Footer/>
     </div>
   );
