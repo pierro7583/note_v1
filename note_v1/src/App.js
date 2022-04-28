af6bfc02f09bc9727c44fd4f0aaa95742db0eaa8
pierro7583 <93189698+pierro7583@users.noreply.github.com>
2022-04-28 18:17:18 +0200

    Update functionality
    Crée une constante qui garde une trace du titre et du contenu.
    Renvoyez la nouvelle note à l'application.

--- note_v1/src/App.js
+++ note_v1/src/App.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
 
 function App() {
+
+
+function addNote(note) {
+console.log(note);
+}
+
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
+
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- note_v1/src/components/CreateArea.jsx
+++ note_v1/src/components/CreateArea.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+//- Crée une constante qui garde une trace du titre et du contenu.
+const [note, setNote] = useState ({
+title: "",
+content: ""
+
+});
+
+function handleChange(event){
+const {name,value} = event.target;
+
+setNote(prevNote =>{
+  return {
+    // spread operator
+    ...prevNote,
+    [name]: value
+  }
+})
+}
+
+//function envoie note lors du click + evite le reload a chaque click
+function submitNote(event){
+  props.onAdd(note);
+event.preventDefault();
+}
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+
+        <textarea 
+        name="content" onChange={handleChange} 
+         value={note.content} 
+         placeholder="Take a note..." 
+         rows="3" />
+
+{/* Trigger submit note */}
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
